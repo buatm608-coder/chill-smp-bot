e3547ac6bd6ef22dcf1d5bc6008f1fd849ec3bc5
RullRajaIbelis <buatm608@gmail.com>
2026-06-13 17:28:53 +0700

    add chanel id

--- bot.py
+++ bot.py
@@ -2,18 +2,33 @@ import discord
 from discord.ext import commands
 from mcstatus import BedrockServer
 import os
+import asyncio
 
 TOKEN = os.environ.get("TOKEN")
 SERVER_IP = "bedrock-2.mcserver.my.id:59516"
+CHANNEL_ID = 1513845505807876107
 
 intents = discord.Intents.default()
 intents.message_content = True
 
 bot = commands.Bot(command_prefix="!", intents=intents)
 
+async def update_channel():
+    await bot.wait_until_ready()
+    channel = bot.get_channel(CHANNEL_ID)
+    while not bot.is_closed():
+        try:
+            server = BedrockServer.lookup(SERVER_IP)
+            status = server.status()
+            await channel.edit(name=f"🟢 Player: {status.players.online}/{status.players.max}")
+        except:
+            await channel.edit(name=f"🔴 Server Offline")
+        await asyncio.sleep(60)
+
 @bot.event
 async def on_ready():
     print(f"Bot online sebagai {bot.user}")
+    bot.loop.create_task(update_channel())
 
 @bot.command()
 async def mcstatus(ctx):
@@ -33,7 +48,7 @@ async def mcstatus(ctx):
             title="🎮 CHILL SMP Status",
             color=discord.Color.red()
         )
-        embed.add_field(name="Status", value="🔴 Offline", inline=False)
+        embed.add_field(name="Status", value="🔴 Server Offline", inline=False)
         await ctx.send(embed=embed)
 
 bot.run(TOKEN)
